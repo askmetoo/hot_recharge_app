f3773952529fcec3e68bea9de6fad828cefea2bb
DonnC <donnclab@gmail.com>
2021-12-09 17:47:50 +0200

    added docs added app on desk

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Hot Recharge App in Frappe ERPNEXT
 
+### Installation
+2. bench get-app --branch master hatchpro_management  https://github.com/HatchproHatcheries/HatchproERP.git
+
+3. bench --site hatchpro.com install-app hatchpro_management
+
+
+### Demo
+![demo.png](screenshots/home.png)
+
 #### License
 
 MIT
--- hot_recharge_app/config/docs.py
+++ hot_recharge_app/config/docs.py
@@ -2,9 +2,9 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/hot_recharge_app"
+# source_link = "https://github.com/DonnC/hot_recharge_app"
 # docs_base_url = "https://[org_name].github.io/hot_recharge_app"
-# headline = "App that does everything"
+# headline = ""
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
